298eadedbcbb21cd354b9e6fc2ff08540db5d3ca
igo95862 <igo95862@yandex.ru>
2014-09-05 21:07:46 +0400

    added xy to npc log

--- scripts/vscripts/developer.lua
+++ scripts/vscripts/developer.lua
@@ -4,7 +4,7 @@ print("developer loading")
 function log_npc( event )
 	local index = event.entindex
 	local unit = EntIndexToHScript(index)
-	print("Index: "..index.." Name: "..unit:GetName().." Created time: "..GetSystemTime())
+	print("Index: "..index.." Name: "..unit:GetName().." Created time: "..GetSystemTime().." at x= "..unit:GetOrigin().x.." y= "..unit:GetOrigin().y)
 end
 
 
