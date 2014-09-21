ac1f3f67dfefde34cf1699284ed9f399b2c37a7d
igo95862 <igo95862@yandex.ru>
2014-09-21 14:28:24 -0800

    Final boss. Added powerup and void spell and AI
    Will cast void on lowest effective HP target and focus it.

--- scripts/vscripts/ai.lua
+++ scripts/vscripts/ai.lua
@@ -1,7 +1,48 @@
 print("AI is loading")
 
+function physical_ehp( unit )
+	return unit:GetHealth() * ((0.06 * unit:GetPhysicalArmorValue()) / (1 + 0.06 * unit:GetPhysicalArmorValue()))
+end
+
+
 function final_boss_think( event )
 	local boss = event.caster
+	local heroes_around = FindUnitsInRadius( DOTA_TEAM_NEUTRALS, boss:GetAbsOrigin(), nil, 1000, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_CLOSEST, false)
+	local void_spell = boss:FindAbilityByName("final_boss_void")
+
+	if void_spell:IsFullyCastable() == true and heroes_around[1] ~= nil then
+		local target = heroes_around[1]
+		local target_ehp = physical_ehp(target)
+
+		for key, hero in pairs(heroes_around) do 
+			local filtered_ehp = physical_ehp(hero)
+			if filtered_ehp < target_ehp then
+				target = hero
+				target_ehp = filtered_ehp
+			end 
+		end
+
+		boss:CastAbilityOnTarget(target, void_spell, -1)
+
+		boss:SetForceAttackTarget(target)
+
+		--boss:MoveToTargetToAttack(target) 
+		--[[ExecuteOrderFromTable({
+								UnitIndex = boss:entindex(), 
+ 								OrderType = DOTA_UNIT_ORDER_ATTACK_TARGET,
+ 		 						TargetIndex = target:entindex()
+								})]]
+		
+	else
+		boss:SetForceAttackTarget(nil)
+	end
+
+	if boss:HasModifier("final_boss_powerup") == false then
+		if (boss:GetHealth() / boss:GetMaxHealth()) <= 0.5 then
+			boss:AddAbility("final_boss_buff")
+			boss:FindAbilityByName("final_boss_buff"):SetLevel(1)
+		end
+	end
 end
 
 function final_boss_defeat( event )
