32b3f4140da4e1e93644519453dcd35bd1a6d281
MNoya <martinnoya@gmail.com>
2014-10-25 19:23:54 -0200

    added statcollectionRPG draft

--- scripts/vscripts/addon_game_mode.lua
+++ scripts/vscripts/addon_game_mode.lua
@@ -9,7 +9,8 @@ require( 'ai' )
 require( 'spawn' )
 local hints = require( 'hints' )
 require('lib.statcollection')
-local JSON = require('lib.json')
+require('lib.statcollectionRPG')
+--local JSON = require('lib.json')
 
 if Convars:GetBool("developer") == true then
 	require( "developer" )
@@ -72,7 +73,7 @@ function Warchasers:InitGameMode()
 	--GameRules:SetHeroRespawnEnabled(false)
 
 	Convars:RegisterCommand( "test", function(...) return Warchasers:SetHeroData( 0 ) end, "Test Command", FCVAR_CHEAT )
-	Convars:RegisterCommand( "data", function(...) return Warchasers:CheckHeroData() end, "Test Command", FCVAR_CHEAT )
+	Convars:RegisterCommand( "data", function(...) return statcollectionRPG.LoadData() end, "Test Command", FCVAR_CHEAT )
 
 	print( "GameRules set" )
 
@@ -1504,7 +1505,7 @@ function Warchasers:SetHeroData( playerID )
 
 end
 
-function Warchasers:CheckHeroData()
+--[[function Warchasers:CheckHeroData()
 	print("Checking Data")
 	local path = "C:\\Program Files (x86)\\Steam\\SteamApps\\common\\dota 2 beta\\dota_ugc\\game\\dota_addons\\warchasers\\scripts\\vscripts\\string.txt"
 	local myString = nil
@@ -1535,35 +1536,144 @@ function Warchasers:CheckHeroData()
 --   or
 --     { "Larry", "Curly", "Moe" }
 
-	for k,v in pairs(result) do
+	--for k,v in pairs(result) do
 		--print(k,v)
-		--[[
-			rounds	table: 0x03a17818  -- << here is the info
-			modID	07dac9699d6c9b7442f8ee7c18c18126
-			duration	145.9688873291
-			matchID	756e28213df345024dc77c7732226cca
-		]]
-	end
+		
+			--rounds	table: 0x03a17818  -- << here is the info
+			--modID	07dac9699d6c9b7442f8ee7c18c18126
+			--duration	145.9688873291
+			--matchID	756e28213df345024dc77c7732226cca
+		
+	--end
 
-	for k2,v2 in pairs(result.rounds.players) do
-		for k3,v3 in pairs(result.rounds.players[k2]) do
-			print(k3,v3)
-			--[[]
-				items	table: 0x03949570
-				hero	table: 0x039857f0
+	for k,v in pairs(result.rounds.players) do
+		for key,values in pairs(result.rounds.players[k]) do
+			print(key,values) --each player
+			--
+				items	table: 0x03949570 << here
+				hero	table: 0x039857f0 << here
 				slotID	1
 				teamID	2
-				abilities	table: 0x0391f810
+				abilities	table: 0x0391f810 << here
 				steamID32	86718505
 				playerName	Noya
-			]]
-			for k4,v4 in pairs(result.rounds.players[k2].items) do
-				--print(k4,v4)
+			
+			if result.rounds.players[k].steamID32 == PlayerResource:GetSteamAccountID(0) then --load for that particular SteamID
+				if key=="items" then
+					Warchasers:ClearItemsForPlayer( 0 )
+					for _,itemData in pairs(values) do --item data
+						--print(itemData.itemName)
+						Warchasers:GiveItemToPlayer( 0, itemData.itemName )
+					end
+				elseif key=="hero" then
+					--print(values.heroID)
+					if values.heroID == PlayerResource:GetSelectedHeroID(0) then --load the data for that particular heroID e.g. 18 = Sven
+						Warchasers:SetLevelsForPlayer(0 , values)
+					end
+				elseif key=="abilities" then
+					for _,abilityData in pairs(values) do --skill data
+						--print(abilityData.abilityName)
+						Warchasers:SetAbilityLevelForPlayer(0, abilityData)
+					end
+				end
 			end
-
 		end
+		print('----------------------')
+	end
+end
+
+function Warchasers:SetLevelsForPlayer( playerID , table)
+	local hero = PlayerResource:GetSelectedHeroEntity(playerID)
+
+	-- Level Up
+	--print(table.level)
+	local current_hero_level = PlayerResource:GetLevel(playerID) --just to be sure we dont level over the stored level
+	local new_hero_level = table.level - current_hero_level
+	for i=1,new_hero_level do
+		hero:HeroLevelUp(false)
 	end
 
+	-- Set Deaths
+	--print(table.deaths)
+	local current_hero_deaths = PlayerResource:GetDeaths(playerID)
+	local new_hero_deaths = table.deaths - current_hero_deaths
+	for i=1,new_hero_deaths do
+		PlayerResource:IncrementDeaths(playerID)
+	end
+
+	-- Set Deaths
+	--print(table.gold)
+	PlayerResource:SetGold(0, table.gold, false)
+
+	-- Set Lasthits
+	--print(table.lastHits)
+	local current_hero_lasthits = PlayerResource:GetLastHits(playerID)
+	local new_hero_lasthits = table.lastHits - current_hero_lasthits
+	for i=1,new_hero_lasthits do
+		PlayerResource:IncrementLastHits(playerID)
+	end
+
+	-- Set Assists
+	--print(table.assists)
+	local current_hero_assists = PlayerResource:GetAssists(playerID)
+	local new_hero_assists = table.assists - current_hero_assists
+	for i=1,new_hero_assists do
+		PlayerResource:IncrementAssists(playerID)
+	end
+
+	-- Set Kills
+	--print(table.kills)
+	local current_hero_kills = PlayerResource:GetKills(playerID)
+	local new_hero_kills = table.kills - current_hero_kills
+	for i=1,new_hero_kills do
+		PlayerResource:IncrementKills(playerID,new_hero_kills)
+	end
+
+	-- Set Denies
+	--print(table.denies)
+	local current_hero_denies = PlayerResource:GetDenies(playerID)
+	local new_hero_denies = table.denies - current_hero_denies
+	for i=1,new_hero_denies do
+		PlayerResource:IncrementDenies(playerID)
+	end
+end
+
+function Warchasers:SetAbilityLevelForPlayer( playerID , abilityData)
+	local hero = PlayerResource:GetSelectedHeroEntity(playerID)
+
+	--print("Setting Skill Level of " .. abilityData.abilityName)
+    -- Grab an ability
+    local ab = hero:FindAbilityByName(abilityData.abilityName)
+
+    -- Check if it is valid
+    if IsValidEntity(ab) then
+        -- Set Level of the ability
+        ab:SetLevel(abilityData.level)
+    end	
+end
+
+function Warchasers:ClearItemsForPlayer( playerID )
+	local hero = PlayerResource:GetSelectedHeroEntity(playerID)
+	local itemCount = 0
+	while itemCount < 12 do
+	    -- Grab an item
+	    local item = hero:GetItemInSlot(itemCount)
+
+	    -- Check if the item is valid
+	    if IsValidEntity(item) then
+	        -- Remove the item
+            hero:RemoveItem(item)
+        end
+
+	    -- Move onto the next item
+	    itemCount = itemCount + 1
+	end
+end
+
+function Warchasers:GiveItemToPlayer( playerID , itemName )
+	local hero = PlayerResource:GetSelectedHeroEntity(playerID)
+	local newItem = CreateItem(itemName, nil, nil)
+	hero:AddItem(newItem)
 end
 
 function Warchasers:RemoveWearables(hero) 
@@ -1588,6 +1698,6 @@ function Warchasers:RemoveWearables(hero)
   for i=1, #wearables do
   	UTIL_RemoveImmediate(wearables[i])
 	end
-end
+end]]
 
 
